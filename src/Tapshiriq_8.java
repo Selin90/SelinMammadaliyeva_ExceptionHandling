9431be2c8eff9e58444e776328645cfc626ac39f
Selin Mammadaliyeva <selin.mammadaliyeva@gmail.com>
2026-07-28 02:18:17 +0400

    Tapshiriq_8 elave edildi

--- src/Tapshiriq_8.java
+++ src/Tapshiriq_8.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tapshiriq_8 {
+
+    public static void main(String[] args) {
+        // 1. HashMap yarat: filial adı, günlük müştəri sayı
+        HashMap<String, Integer> filiallar = new HashMap<>();
+        filiallar.put("Nərimanov", 45);
+        filiallar.put("Yasamal", 60);
+        filiallar.put("Sebail", 30);
+        filiallar.put("Xetai", 55);
+
+        // 2. Cüt sayını çap et
+        System.out.println("Cüt sayı: " + filiallar.size());
+
+        // 3. Dövr ilə hər cütü "filial, say" formatında çap et
+        for (Map.Entry<String, Integer> cut : filiallar.entrySet()) {
+            System.out.println(cut.getKey() + " → " + cut.getValue());
+        }
+
+        // 4. Bir filial bağlanır - bazadan sil
+        filiallar.remove("Sebail");
+
+        // 5. Cüt sayını yenidən çap et
+        System.out.println("Yeni cüt sayı: " + filiallar.size());
+    }
 }
